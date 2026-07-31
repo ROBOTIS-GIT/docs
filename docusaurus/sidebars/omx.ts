ef9f3d3f10d2543c04783749e4e5040c78b37c60
Hyungyu Kim <kimhg@robotis.com>
2026-07-31 16:55:40 +0900

    update omx, omy template

--- docusaurus/sidebars/omx.ts
+++ docusaurus/sidebars/omx.ts
@@ -34,6 +34,19 @@ const omxSidebar: SidebarsConfig[string] = [
         link: {type: 'doc', id: 'systems/omx/quick_start_guide/operation_guide'},
         collapsed: false,
         items: [
+          {
+            type: 'category',
+            label: 'Cyclo Manager',
+            link: {type: 'doc', id: 'systems/omx/quick_start_guide/operation_guide/cyclo_manager/cyclo_manager'},
+            collapsed: false,
+            items: [
+              {type: 'doc', id: 'systems/omx/quick_start_guide/operation_guide/cyclo_manager/install', label: 'Install'},
+              {type: 'doc', id: 'systems/omx/quick_start_guide/operation_guide/cyclo_manager/bringup', label: 'Bringup'},
+              {type: 'doc', id: 'systems/omx/quick_start_guide/operation_guide/cyclo_manager/manage_container', label: 'Manage Containers'},
+              {type: 'doc', id: 'systems/omx/quick_start_guide/operation_guide/cyclo_manager/version_update', label: 'Version Update'},
+              {type: 'doc', id: 'systems/omx/quick_start_guide/operation_guide/cyclo_manager/novnc', label: 'noVNC'},
+            ],
+          },
           {type: 'doc', id: 'systems/omx/quick_start_guide/operation_guide/teleoperation', label: 'Teleoperation'},
           {type: 'doc', id: 'systems/omx/quick_start_guide/operation_guide/robot_control', label: 'Robot Control'},
         ],
