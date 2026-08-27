6f55af9e4be2f2ca07528c478c1a088bd8ba7947
Seongwoo Kim <kimsw@robotis.com>
2026-08-27 10:52:21 +0900

    Streamline Navigation guide and remove Easy Navi
    Signed-off-by: Seongwoo Kim <kimsw@robotis.com>

--- docusaurus/sidebars/aiworker.ts
+++ docusaurus/sidebars/aiworker.ts
@@ -116,7 +116,6 @@ const aiworkerSidebar: SidebarsConfig[string] = [
       {type: 'doc', id: 'systems/aiworker/advanced_features/advanced_features', label: 'Overview', key: 'advanced-features-overview'},
       {type: 'doc', id: 'systems/aiworker/advanced_features/cyclo_control', label: 'Cyclo Control'},
       {type: 'doc', id: 'systems/aiworker/advanced_features/robotis_vuer', label: 'ROBOTIS Vuer'},
-      {type: 'doc', id: 'systems/aiworker/advanced_features/eazy_navi', label: 'Easy Navi'},
     ],
   },
   {
