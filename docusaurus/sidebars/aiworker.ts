c130c168f82a5249ce18ecec26beb3592e2a16c1
Hyunwoo Nam <nhw@robotis.com>
2026-09-14 15:29:14 +0900

    Initial format
    Signed-off-by: Hyunwoo Nam <nhw@robotis.com>

--- docusaurus/sidebars/aiworker.ts
+++ docusaurus/sidebars/aiworker.ts
@@ -153,6 +153,7 @@ const aiworkerSidebar: SidebarsConfig[string] = [
           {type: 'doc', id: 'systems/aiworker/resources/technical_story/nav2', label: 'Nav2'},
           {type: 'doc', id: 'systems/aiworker/resources/technical_story/isaac_vr_teleoperation', label: 'Isaac VR Teleoperation'},
           {type: 'doc', id: 'systems/aiworker/resources/technical_story/isaac_centerpose', label: 'Isaac CenterPose'},
+          {type: 'doc', id: 'systems/aiworker/resources/technical_story/isaac_cumotion', label: 'Isaac cuMotion'},
         ],
       },
     ],
